183a5ced537aa3f8939793c8fd1926c402d326f1
togi06 <t_bayter_06@hotmail.com>
2022-12-10 21:29:22 +0100

    fixed the main  method

--- src/Test1.java
+++ src/Test1.java
@@ -1,9 +1,8 @@
 public class Test1  {
-    public static void main(String args) {
+    public static void main(String[] args) {
 
     }
-slkjdakl jdlşsajd şaskhd kjadkjash dkadjl
-    kdflkdhşfkjdsa
+
 
 }
 
